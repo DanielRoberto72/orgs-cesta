f394c369abc469e9b19ea4e2fac893daff88f538
Daniel Roberto Pereira <daniel.roberto@surf.com.br>
2022-07-13 21:12:47 -0300

    feat: adicionando imagem, estilo e fontes na pagina de cesta

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -1,7 +1,19 @@
 import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fonte] = useFonts({
+    "mRegular": Montserrat_400Regular,
+    "mBold": Montserrat_700Bold
+  })
+
+  if (!fonte){
+    return(
+      <View></View>
+    )
+  }
+
   return (
     <SafeAreaView>
       <StatusBar/>
--- orgs-cesta/src/telas/Cesta.js
+++ orgs-cesta/src/telas/Cesta.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, Dimensions, Image, Text} from "react-native";
+import { StyleSheet, Dimensions, Image, Text, View} from "react-native";
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -8,8 +9,19 @@ export default function Cesta(){
 return(
     <>
     <Image source={topo} style={estilos.topo} />
-    <Text>Detalhas da cesta</Text>
+    <Text style={estilos.titulo} >Detalhe da cesta</Text>
+    <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de verduras</Text>
+        <View style={estilos.fazenda}>
+            <Image source={logo} style={estilos.imagemfazenda}/>
+            <Text style={estilos.nomeFazenda}>Surf farm</Text>
+        </View>
+        <Text style={estilos.descricao}>Uma cesta com produtos cuidadosamente cultivados e selecionados</Text>
+        <Text style={estilos.preco}>R$ 40.00</Text>
+    </View>
     </>
+
+    
     
     )
 }
@@ -17,6 +29,53 @@ return(
 const estilos = StyleSheet.create({
     topo:{
         width: "100%",
-        heigth: 578 / 768 * width
+        height: 578 / 768 * width
+    },
+    titulo:{
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "#fff",
+        fontWeight: "bold",
+        padding: 50
+    },
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome:{
+        color: "#464646",
+        fontSize:26,
+        lineHeight:42,
+        fontFamily: "mBold"
+    },
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+    imagemfazenda:{
+        width: 32,
+        height: 32,
+        
+    },
+    nomeFazenda:{
+        fontSize:16,
+        lineHeight:26,
+        marginLeft: 12,
+        fontFamily: "mRegular"
+    },
+    descricao:{
+        color: "#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco:{
+        color: "#2A9f85",
+        fontWeight: "bold",
+        fontSize:26,
+        lineHeight: 42,
+        marginTop:8
     }
 })
