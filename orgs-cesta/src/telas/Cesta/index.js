48a376eb11ab9c292daeb0c10b257133e4f70546
Daniel Roberto Pereira <daniel.roberto@surf.com.br>
2022-07-18 21:09:16 -0300

    feat: adicionando lista de itens

--- orgs-cesta/src/telas/Cesta/index.js
+++ orgs-cesta/src/telas/Cesta/index.js
@@ -3,15 +3,17 @@ import { StyleSheet, Image, Text, View} from "react-native";
 
 import Topo from "./componentes/Topo";
 import Detalhes from "./componentes/Detalhes";
+import Itens from "./componentes/Itens";
 
 
 
-export default function Cesta({topo, detalhes}){
+export default function Cesta({topo, detalhes, itens}){
 return(
     <>
     <Topo {...topo}/>
     <View style={estilos.cesta}>
-        <Detalhes {...detalhes}/>
+        <Detalhes {...detalhes} />
+        <Itens {...itens} />
     </View>
     </>  
     )
